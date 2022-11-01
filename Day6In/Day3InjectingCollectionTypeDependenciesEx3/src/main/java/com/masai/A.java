727f3b63b81f37e3d7b2ce55662dc1f07b538f83
Vijay-Prakash146246 <vp146246@gmail.com>
2022-11-01 12:48:35 +0530

    Dependency Example get added

---
+++ Day6In/Day3InjectingCollectionTypeDependenciesEx3/src/main/java/com/masai/A.java
@@ -0,0 +1,17 @@
+package com.masai;
+
+import java.util.Map;
+
+public class A 
+{
+	private Map<Student, String> theMap;
+	public void setTheMap(Map<Student,String> theMap) 
+	{
+	this.theMap = theMap;
+	}
+	public void show() 
+	{
+	System.out.println("inside showA of A ");
+	System.out.println(theMap);
+	}
+}
