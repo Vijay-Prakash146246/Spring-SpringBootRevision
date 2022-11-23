2b11fdb5036e9ba7c30277e00d23970433d9dc37
Vijay-Prakash146246 <vp146246@gmail.com>
2022-11-23 16:26:59 +0530

    Saving List of Student get added

--- springDataJpaEx/src/main/java/com/masai/Application.java
+++ springDataJpaEx/src/main/java/com/masai/Application.java
@@ -5,6 +5,9 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
 
+import java.util.ArrayList;
+import java.util.List;
+
 @SpringBootApplication
 public class Application
 {
@@ -20,7 +23,22 @@ public class Application
 					"Prakash",
 					"vijayPrakash12345@gmail.com",
 					21);
-			studentRepository.save(vijay);
+
+			//creating list of student
+			Student s1 = new Student("Sonu","Kumar","sonu@gmail.com",24);
+			Student s2 = new Student("Monu","Kumar","Monu@gmail.com",25);
+			Student s3 = new Student("Niraj","Kumar","Niraj@gmail.com",26);
+			Student s4 = new Student("Amit","Kumar","Amit@gmail.com",27);
+			Student s5 = new Student("Sumit","Kumar","Sumit@gmail.com",28);
+			List<Student> students = new ArrayList<Student>();
+			//students.add(vijay);
+			students.add(s1);
+			students.add(s2);
+			students.add(s3);
+			students.add(s4);
+			students.add(s5);
+			//studentRepository.save(vijay);
+			studentRepository.saveAll(students);
 		};
 	}
 }
